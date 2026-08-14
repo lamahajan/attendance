342264c4d23522750ea6ea5605114dddf1710e66
Lakshaya Mahajan <lm@lisdnn.org>
2026-08-15 00:16:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
    file badlo to CACHE ka number bhi badha dena.
    ═══════════════════════════════════════════════════════════════ */
 
-const CACHE = 'markd-v13';
+const CACHE = 'markd-v14';
 
 const SHELL = [
   './',
